b8749146cf861d748fa5e8f34d3a44550e7c0ef7
Jonathan <jonathanpalma89@gmail.com>
2019-11-05 21:42:38 +0000

    Removed array & apply hash instead

--- directory.rb
+++ directory.rb
@@ -1,32 +1,32 @@
-# Puts all students into an array
+# Puts all students into an Hash
 students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates",
-  "Jonathan Palma"
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november},
+  {name: "Jonathan Palma", cohort: :november}
 ]
 
 def print_header                                          # Print title
   puts "The students of Villains Academy"
-  puts "--------------------------------"
+  puts "--------------------------------\n"
 end
 
-def print(names)                                          # Print names of Students
+def print(names)                                          # Print names of Students & Cohort
   names.each { |student|
-    puts student
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   }
 end
 
 def print_footer(names)                                   # Print the total of students
-  puts "Overall, we have #{names.count} great students"
+  puts "\nOverall, we have #{names.count} great students"
 end
 
 print_header                                              # Call the methods
