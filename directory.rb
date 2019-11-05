3ab840b55cfdb9d084e3900cdee3fe5a63c2c857
Jonathan <jonathanpalma89@gmail.com>
2019-11-05 22:11:25 +0000

    Create input student method

--- directory.rb
+++ directory.rb
@@ -1,19 +1,3 @@
-# Puts all students into an Hash
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november},
-  {name: "Jonathan Palma", cohort: :november}
-]
-
 def print_header                                          # Print title
   puts "The students of Villains Academy"
   puts "--------------------------------\n"
@@ -29,6 +13,19 @@ def print_footer(names)                                   # Print the total of s
   puts "\nOverall, we have #{names.count} great students"
 end
 
+def input_students
+  puts "Please enter the name of the student\nTo finish, just hit return twice"
+  name = gets.chomp
+  students = []                                           # Create array for future data
+  while !name.empty?
+    students << {name: name, cohort: :november}           # Store the student hash to the Array 
+    puts "Now we have #{students.count}"
+    name = gets.chomp
+  end
+  students
+end
+
+students = input_students
 print_header                                              # Call the methods
 print(students)
 print_footer(students)
